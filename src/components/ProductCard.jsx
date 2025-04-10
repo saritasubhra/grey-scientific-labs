d837bb0824bfb2465e378d6e5e14d9a797ab3024
saritasubhra <saritasubhra1@gmail.com>
2025-04-10 10:51:33 +0530

    products page

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,29 @@
+function ProductCard({ prod }) {
+  const {
+    title,
+    image,
+    price,
+    rating: { rate },
+  } = prod;
+  return (
+    <div className="bg-white rounded-2xl shadow-2xl overflow-hidden hover:scale-105 transition-all duration-300">
+      <div className="h-52  flex items-center justify-center">
+        <img
+          src={image}
+          alt={title}
+          className="h-full w-full object-contain p-4"
+        />
+      </div>
+      <div className="p-5 space-y-2 bg-gray-100">
+        <h1 className="font-bold text-xl text-sky-500 truncate">{title}</h1>
+        <p className="text-3xl font-semibold text-green-600">${price}</p>
+        <p className="text-sm text-gray-500">⭐ {rate} Rating</p>
+        <button className="mt-3 w-full bg-yellow-400 py-2 rounded-xl hover:bg-yellow-500 transition-all duration-200">
+          View Details
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import ProductCard from "../components/ProductCard";
+import Spinner from "../components/Spinner";
+
 function Home() {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const res = await axios.get("https://fakestoreapi.com/products");
+        console.log(res);
+        setProducts(res.data);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  if (!products.length) return <Spinner />;
+  return (
+    <div className="max-w-5xl mx-auto grid grid-cols-3 gap-10">
+      {products.map((prod) => (
+        <ProductCard key={prod.id} prod={prod} />
+      ))}
+    </div>
+  );
 }
 
 export default Home;
